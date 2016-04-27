4b8f0da8a06a37dc544e5bab091e67d150ab9375
Daniel Haehn <haehn@seas.harvard.edu>
2016-04-27 08:48:58 -0400

    Finally a clean structure for the CNN configurations.

--- nets/cnn.py
+++ nets/cnn.py
@@ -5,62 +5,32 @@ from lasagne.updates import nesterov_momentum
 
 from helper import *
 
-class CNN(NeuralNet):
+class CNN(object):
 
-    def __init__(
-            self,
- 
-            **kwargs
-    ):
+    def __init__(self, *args, **kwargs):
+        '''
+        '''
 
-        print kwargs
+        kwargs['update'] = nesterov_momentum
+        kwargs['update_learning_rate'] = 0.001
+        kwargs['update_momentum'] = 0.9
+        # # update_learning_rate=theano.shared(float32(0.03)),
+        # # update_momentum=theano.shared(float32(0.9)),
 
-        NeuralNet.__init__(self, **kwargs,
-                            update=nesterov_momentum,
-                            update_learning_rate=0.001,
-                            update_momentum=0.9,
-                            # update_learning_rate=theano.shared(float32(0.03)),
-                            # update_momentum=theano.shared(float32(0.9)),
-                            loss=None,
-                            objective=objective,
-                            objective_loss_function=None,
-                            batch_iterator_train=MyBatchIterator(batch_size=100),
-                            batch_iterator_test=MyBatchIterator(batch_size=100),
-                            regression=True,
-                            max_epochs=300,
-                            train_split=TrainSplit(eval_size=0.25),
-                            custom_scores=None,
-                            X_tensor_type=None, 
-                            y_tensor_type=None,
-                            use_label_encoder=False,
-                            on_batch_finished=None,
-                            on_epoch_finished=[
-                                # AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
-                                # AdjustVariable('update_momentum', start=0.9, stop=0.999),
-                                EarlyStopping(patience=30),
-                                ],
-                            on_training_started=None,
-                            on_training_finished=None,
-                            more_params=None,
-                            verbose=True)
-                           # update,
-                           # update_learning_rate,
-                           # update_momentum,
-                           # loss,
-                           # objective,
-                           # objective_loss_function,
-                           # batch_iterator_train,
-                           # batch_iterator_test,
-                           # regression,
-                           # max_epochs,
-                           # train_split,
-                           # custom_scores,
-                           # X_tensor_type,
-                           # y_tensor_type,
-                           # use_label_encoder,
-                           # on_batch_finished,
-                           # on_epoch_finished,
-                           # on_training_started,
-                           # on_training_finished,
-                           # more_params,
-                           # verbose)
+        kwargs['regression'] = True
+        kwargs['batch_iterator_train'] = MyBatchIterator(batch_size=100)
+        kwargs['batch_iterator_test'] = MyBatchIterator(batch_size=100)
+        kwargs['max_epochs'] = 500
+        kwargs['train_split'] = TrainSplit(eval_size=0.25)
+        kwargs['on_epoch_finished'] = [
+                # AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+                # AdjustVariable('update_momentum', start=0.9, stop=0.999),
+                EarlyStopping(patience=30),
+            ]
+        
+        kwargs['verbose'] = True
+
+
+        cnn = NeuralNet(*args, **kwargs)
+        self.__class__ = cnn.__class__
+        self.__dict__ = cnn.__dict__        
