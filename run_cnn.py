eab95c0607d5592f175ede755b54a9b7bb503ceb
Daniel Haehn <haehn@seas.harvard.edu>
2016-04-28 14:41:29 -0400

    Add a template for cluster running.

---
+++ run_cnn.py
@@ -0,0 +1,20 @@
+import mlproof as mlp
+import mlproof.nets as nets
+
+import numpy as np
+
+import cPickle as picke
+
+
+
+# create CNN
+
+# load data
+
+# train
+
+# store CNN
+with open(OUTPUT_FILE, 'wb') as f:
+  pickle.dump(cnn, f, -1)
+
+print 'All done.'
