fa53efac775d5f06232121cb41e3021b81eb2735
haehn <haehn@seas.harvard.edu>
2016-04-29 14:17:33 -0400

    Add argparse.

--- run_cnn.py
+++ run_cnn.py
@@ -1,3 +1,5 @@
+import argparse
+
 import mlproof as mlp
 import mlproof.nets as nets
 
@@ -6,41 +8,52 @@ import os
 import cPickle as picke
 
 
-CNN_NAME = 'MergeNet'
-PATCH_PATH = 'cylinder_small1'
-BORDER = 'larger_border_overlap'
-DESC = 'test'
-OUTPUT_FOLDER = os.path.expanduser('~/nets/MergeNet_'+PATCH_PATH+'_'+BORDER+'_'+DESC)
-
-if not os.path.exists(OUTPUT_FOLDER):
-  os.makedirs(OUTPUT_FOLDER)
+if __name__ == '__main__':
 
 
-# create CNN
-cnn = eval("nets."+CNN_NAME)()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-n", "--name", type=str, help="the cnn type", default='MergeNet')
+  parser.add_argument("-p", "--patchpath", type=str, help="the patch folder in the datapath", default='cylinder_small1')
+  parser.add_argument("-b", "--border", type=str, help="the border to use", default='larger_border_overlap')
+  parser.add_argument("-d", "--desc", type=str, help="the description", default='test')
 
-# load data
-X_train, y_train, X_test, y_test = mlp.Patch.load(PATCH_PATH)
+  args = parser.parse_args()
 
-# train
-cnn = cnn.fit({'image_input': X_train['image'],
-               'prob_input': X_train['prob'],
-               'binary_input': X_train['merged_array'],
-               'border_input': X_train[BORDER]},
-               y_train)
+  CNN_NAME = args.name
+  PATCH_PATH = args.patchpath
+  BORDER = args.border
+  DESC = args.desc
+  OUTPUT_FOLDER = os.path.expanduser('~/nets/MergeNet_'+PATCH_PATH+'_'+BORDER+'_'+DESC)
 
-# test
-test_accuracy = cnn.score({'image_input': X_test['image'],
-                           'prob_input': X_test['prob'],
-                           'binary_input': X_test['merged_array'],
-                           'border_input': X_test[BORDER]},
-                           y_test)
+  if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
 
-with open(os.path.join(OUTPUT_PATH, 'test_'+str(test_accuracy)+'.txt'), 'w') as f:
-  f.write(str(test_accuracy))
 
-# store CNN
-with open(os.path.join(OUTPUT_FOLDER, 'net.p'), 'wb') as f:
-  pickle.dump(cnn, f, -1)
+  # create CNN
+  cnn = eval("nets."+CNN_NAME)()
 
-print 'All done.'
+  # load data
+  X_train, y_train, X_test, y_test = mlp.Patch.load(PATCH_PATH)
+
+  # train
+  cnn = cnn.fit({'image_input': X_train['image'],
+                 'prob_input': X_train['prob'],
+                 'binary_input': X_train['merged_array'],
+                 'border_input': X_train[BORDER]},
+                 y_train)
+
+  # test
+  test_accuracy = cnn.score({'image_input': X_test['image'],
+                             'prob_input': X_test['prob'],
+                             'binary_input': X_test['merged_array'],
+                             'border_input': X_test[BORDER]},
+                             y_test)
+
+  with open(os.path.join(OUTPUT_PATH, 'test_'+str(test_accuracy)+'.txt'), 'w') as f:
+    f.write(str(test_accuracy))
+
+  # store CNN
+  with open(os.path.join(OUTPUT_FOLDER, 'net.p'), 'wb') as f:
+    pickle.dump(cnn, f, -1)
+
+  print 'All done.'
